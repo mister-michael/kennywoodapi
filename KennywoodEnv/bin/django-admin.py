e2b9bdaa97b97fa91917c0331032c5a2733eae2a
Michael Clark <michaelclarknashville@protonmail.com>
2020-05-19 14:03:16 -0500

    first commit, end chapter 4 book 4

---
+++ KennywoodEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/d.a.n.k.e./workspace/backEnd/book4/kennywood/KennywoodEnv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
